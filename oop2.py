8b6fea5f1cd65310b8ba536235d74319cd88076a
airmig <airmig@gmail.com>
2021-09-20 11:26:22 -0600

    added new policy}

--- oop2.py
+++ oop2.py
@@ -11,12 +11,17 @@ def print_dict(d):
 try:
     logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(process)d - %(levelname)s - %(message)s")
     workers = employees.employee_database.employees()
-    productivity.track(workers, 40)
-    hr.calculate_payroll(workers)
+
 
     temp_secretary = employees.Employee(5)
     logging.info(dir(temp_secretary))
     logging.info(print_dict(temp_secretary.to_dict()))
 
+    sales_employee = workers[2]
+    ltd_policy = hr.LTDPolicy()
+    sales_employee.apply_payroll_policy(ltd_policy)
+
+    productivity.track(workers, 40)
+    hr.calculate_payroll(workers)
 except:
     logging.exception("Fatal error")
